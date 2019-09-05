7566b4feffa820b21474107a6965d2a6cc030527
Bernardo Quintino <b.quintinoguzman@gmail.com>
2019-09-05 17:51:52 -0500

    Pequeñas modificaciones al script

--- BDPro.sql
+++ BDPro.sql
@@ -1,31 +1,34 @@
+﻿DROP DATABASE IF EXISTS ProyectoCN;
 CREATE DATABASE ProyectoCN;
 USE ProyectoCN;
 
 CREATE TABLE Empleado(
 Id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-Nombre VARCHAR (59),
-Apellido_paterno VARCHAR (50),
+Nombre VARCHAR (50) NOT NULL,
+Apellido_paterno VARCHAR (50) NOT NULL,
 Apellido_materno VARCHAR (50),
-Crup VARCHAR (50),
-Estatus BIT,
-Puesto ENUM ('Genernte','Secretaria','Producción', 'Finanzas', 'Logistica')
-);
+Curp VARCHAR (18),
+Correo VARCHAR (30),
+Puesto ENUM ('Generente','Secretaria','Producción', 'Finanzas', 'Logistica'),
+Estatus BIT
+)AUTO_INCREMENT=100000;
 
 CREATE TABLE Usuario(
 Id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 Id_empleado INT NOT NULL,
-User_name VARCHAR (50),
-pass VARCHAR (40),
+User_name VARCHAR (50) NOT NULL,
+pass VARCHAR (50) NOT NULL,
 Estatus ENUM ('Dios','Semidios', 'Mortal'),
 INDEX (Id_empleado),
 FOREIGN KEY (Id_empleado) REFERENCES Empleado(Id)
-);
+)AUTO_INCREMENT=300000;
 
 CREATE TABLE Registro(
 Id_empleado INT NOT NULL,
-INDEX (Id_empleado),
-FOREIGN KEY (Id_empleado) REFERENCES Empleado(Id),
-Hora_entrada DATETIME,
+Hora_entrada DATETIME NOT NULL,
 Hora_salida DATETIME,
-Estatus BIT
+Activo BIT NOT NULL,
+Estatus BIT,
+INDEX (Id_empleado),
+FOREIGN KEY (Id_empleado) REFERENCES Empleado(Id)
 );
