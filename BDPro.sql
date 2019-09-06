a88825b5ec7c20d27f2bab50f37342c9d418f2e5
Adolfo <ja.gutierrez.gaytan@ugto.mx>
2019-09-06 18:55:19 -0500

    Inserciones en la base de datos
    InsercionesBD

--- BDPro.sql
+++ BDPro.sql
@@ -1,4 +1,4 @@
-﻿DROP DATABASE IF EXISTS ProyectoCN;
+DROP DATABASE IF EXISTS ProyectoCN;
 CREATE DATABASE ProyectoCN;
 USE ProyectoCN;
 
@@ -9,9 +9,19 @@ Apellido_paterno VARCHAR (50) NOT NULL,
 Apellido_materno VARCHAR (50),
 Curp VARCHAR (18),
 Correo VARCHAR (30),
-Puesto ENUM ('Generente','Secretaria','Producción', 'Finanzas', 'Logistica'),
+Puesto ENUM ('Gerente','Secretaria','Producción', 'Finanzas', 'Logistica'),
 Estatus BIT
 )AUTO_INCREMENT=100000;
+
+INSERT INTO Empleado VALUES (DEFAULT,'Maria','Guzman','Lopez','12JKJKDS','maria@gmail.com','Secretaria',1);
+INSERT INTO Empleado VALUES (DEFAULT,'Juan','Hernandez','Torres','13kjkldsj','juan@gmail.com','Gerente',1);
+INSERT INTO Empleado VALUES (DEFAULT,'Jose','Dominguez','Calderon','12uroeuie','Jose@gmail.com','Producción',1);
+INSERT INTO Empleado VALUES (DEFAULT,'Guadalupe','Estrada','Gonzalez','3dsfsdf','Lupita@gmail.com','Secretaria',1);
+INSERT INTO Empleado VALUES (DEFAULT,'Jennifer','Gutierrez','Constantino','434sdfdf','Jenny@gmail.com','Finanzas',1);
+INSERT INTO Empleado VALUES (DEFAULT,'Rosario','Gaytan','Segura','rwrewrs','ros@gmail.com','Gerente',1);
+INSERT INTO Empleado VALUES (DEFAULT,'Jesus','Contreras','Gaytan','1werdsf','jesus@gmail.com','Logistica',1);
+
+SELECT * FROM Empleado;
 
 CREATE TABLE Usuario(
 Id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -23,6 +33,16 @@ INDEX (Id_empleado),
 FOREIGN KEY (Id_empleado) REFERENCES Empleado(Id)
 )AUTO_INCREMENT=300000;
 
+INSERT INTO Usuario VALUES (DEFAULT, 100000,'Maria_3', 'daddsade','Semidios');
+INSERT INTO Usuario VALUES (DEFAULT, 100001,'Juan_H', 'reterf','Dios');
+INSERT INTO Usuario VALUES (DEFAULT, 100002,'Jose1', 'dfsdkl','Mortal');
+INSERT INTO Usuario VALUES (DEFAULT, 100003,'Lupita_E', 'rwerwe','Semidios');
+INSERT INTO Usuario VALUES (DEFAULT, 100004,'Jenny_4', 'w3423lk','Semidios');
+INSERT INTO Usuario VALUES (DEFAULT, 100005,'Ross_G', '34klkjkl','Dios');
+INSERT INTO Usuario VALUES (DEFAULT, 100006,'Jesus_C', 'kjk323','Semidios');
+
+SELECT * FROM Usuario;
+
 CREATE TABLE Registro(
 Id_empleado INT NOT NULL,
 Hora_entrada DATETIME NOT NULL,
@@ -32,3 +52,13 @@ Estatus BIT,
 INDEX (Id_empleado),
 FOREIGN KEY (Id_empleado) REFERENCES Empleado(Id)
 );
+
+INSERT INTO Registro VALUES(100000, '2019-11-24 17:15:10', '2013-11-24 17:20:10',1,1);
+INSERT INTO Registro VALUES(100001, '2019-11-24 17:15:10', '2013-11-24 17:20:10',1,1);
+INSERT INTO Registro VALUES(100002, '2019-11-24 17:15:10', '2013-11-24 17:20:10',1,1);
+INSERT INTO Registro VALUES(100003, '2019-11-24 17:15:10', '2013-11-24 17:20:10',1,1);
+INSERT INTO Registro VALUES(100004, '2019-11-24 17:15:10', '2013-11-24 17:20:10',1,1);
+INSERT INTO Registro VALUES(100005, '2019-11-24 17:15:10', '2013-11-24 17:20:10',1,1);
+INSERT INTO Registro VALUES(100006, '2019-11-24 17:15:10', '2013-11-24 17:20:10',1,1);
+
+SELECT * FROM Registro;
