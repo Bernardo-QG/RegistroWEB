f5e9be94ee0f046053940d2e15409a6f076809d7
Unknown <jj.olmoszavala@ugto.mx>
2019-09-12 21:31:38 -0500

    Borrado de usuarios y empleados (updates)

---
+++ pagina web(Pruebas PHP-BD)/borraPHP.php
@@ -0,0 +1,37 @@
+<?php 
+	//Variables de conexion
+	$hostname="localhost";
+	$username="root";
+	$password="";
+	$dbname="proyectocn";
+	//Cadena de conexion
+	$conexion=new mysqli($hostname, $username, $password, $dbname);
+	//Probar conexion
+	if ($conexion->connect_error) 
+	{
+    	die("Error: " . $conexion->connect_error);
+	}
+	//Obtener datos de Form HTML
+	$idEmpleado=$_POST['idEmpleado'];
+	//Insertar en base de datos
+	$consulta="update usuario set Estatus=0 where Id_empleado=".$idEmpleado.";";
+	//Ejecutar consulta
+	if ($conexion->query($consulta) === TRUE) 
+	{
+    	echo "Deleted successfully";
+	}
+	else 
+	{
+    	echo "Error: " . $consulta . "<br>" . $conexion->error;
+	}
+	$consulta="update empleado set Estatus=0 where Id=".$idEmpleado.";";
+	if ($conexion->query($consulta) === TRUE) 
+	{
+    	echo "Deleted successfully";
+	}
+	else 
+	{
+    	echo "Error: " . $consulta . "<br>" . $conexion->error;
+	}
+	$conexion->close();
+ ?>
