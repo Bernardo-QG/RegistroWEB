266ee055956e5a0af36925aaedc76af67547a4a9
Bernardo Quintino <b.quintinoguzman@gmail.com>
2019-10-06 19:23:11 -0500

    olvido

---
+++ JavaScript/ActivoEmpleado.js
@@ -0,0 +1,31 @@
+function Activos(){
+    $('#tbodyDatos').empty();
+    $.get("PHP/Activos.php?fecha="+$("#txtfecha").val()+"&idEmpleado="+$("#txtIdEmpleado").val(),function(data){
+       if(data!="No"){
+        gct=JSON.parse(data);
+        //alert(gct);
+        for(var i=0; i<gct["AllActivos"].length; i++){
+            //alert("Dato");
+            $('#activos > tbody:last-child').append(' '
+            +'<tr><td >'+gct["AllActivos"][i].Id_empleado+'</td>'
+            +'<td >'+gct["AllActivos"][i].Hora_entrada+'</td>'
+            +'<td >'+gct["AllActivos"][i].Hora_salida+'</td>'
+            +'<td >'+((gct["AllActivos"][i].Activo==1)?"Si":"No")+'</td>'
+         //   +'<td >'
+           // +'<button onclick="Actualizar('+gct["AllActivos"][i].Id_empleado+')" type="button" class="btn btn-info"><i class="fas fa-close"></i></button>'
+            //+'</td>'
+            +'</tr>');               
+            
+        }
+    }
+    
+      });
+      
+}
+function ini(){    
+  $.get("PHP/Sesion.php?funcion=getlog&permiso=",function(result){ 
+      if(result=="Mortal" || result=="0" ){          
+          window.open('inicio.html','_self'); 
+      }
+  });    
+}
